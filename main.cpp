fac6c9b9d82bc544434c244cbd4bd6733792c932
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2023-07-06 09:26:54 -0400

    Add files via upload
    SFML works now

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+
+#include <fstream>
+#include <iostream>
+
+#include "header.h"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void game()
+{
+    //Main game loop
+    while (true)
+    {
+
+    }
+}
+int main() {
+
+    /*
+     * Chess game needs:
+     * 1) Main menu
+     * 2) Start button
+     * 3) Start over button
+     * 4) Just do multiplayer for now?
+     * 5) Add chess AI later
+     * */
+
+    game();
+
+
+    return 0;
+}
