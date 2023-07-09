9093fa5e4f8f08e3ef8b0993eb6ea6775e8d219a
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2023-07-09 09:22:56 -0400

    Update header.h
    Added Tiles and Board classes

--- header.h
+++ header.h
@@ -48,8 +48,50 @@ public:
 
 };
 
-//Class for buttons on main screen
+enum class Color {White, Black, None};
+enum class Type {Queen, King, Bishop, Knight, Rook, Pawn, None};
 
 //Class for each board tile
+class Tiles
+{
+	//Using enumeration to specify color/type of pieces, and whether or not they are on the board
+	Color pieceColor;
+	Type pieceType;
+
+public:
+
+	//Tiles constructor
+	Tiles(Color color, Type type)
+	{
+		pieceColor = color;
+		pieceType = type;
+	};
+
+	//Function to set position of tile images
+	void setPosition(int x, int y)
+	{
+
+	}
+
+};
 
 //Class for board
+class Board
+{
+	//Create a vector of tiles to represent the board;
+	vector<vector<Tiles>> tileVector;
+
+public:
+
+	//Board constructor
+	Board()
+	{
+		//Create a tile vector with 64 tiles, and each tile has None color, None type
+		tileVector.resize(8, vector<Tiles>(8, Tiles(Color::None, Type::None)));
+	}
+
+};
+
+//Class for buttons on main screen
+
+
