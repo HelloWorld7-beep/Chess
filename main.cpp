38d2bb3b1af09b4984378243fa41a55b53bab5f2
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2023-07-11 09:52:39 -0400

    Update main.cpp
    Rendered window, initialized board class in main

--- main.cpp
+++ main.cpp
@@ -14,9 +14,22 @@ using namespace std;
 
 void game()
 {
+    //Create window
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Chess");
+
+    //Create board
+    Board board();
+
     //Main game loop
     while (true)
     {
+        
+
+
+
+
+
+
 
     }
 }
@@ -24,7 +37,7 @@ int main() {
 
     /*
      * Chess game needs:
-     * 1) Main menu
+     * 1) Main menu screen
      * 2) Start button
      * 3) Start over button
      * 4) Just do multiplayer for now?
