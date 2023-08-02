a4d0f86832d8a051118901cf007d7f5d44fd7138
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2023-08-02 09:45:02 -0400

    Update main.cpp
    Added player tracking/piece clicking functionality

--- main.cpp
+++ main.cpp
@@ -28,88 +28,126 @@ void game()
     //Create event
     sf::Event event;
 
-    // Main game loop, while the window is open
+    //Boolean for which player is playing
+    bool player = false;
+
+    window.clear(sf::Color::White);
+
+    // Loop through each tile and draw the corresponding chess piece
+    for (int row = 0; row < 8; row++)
+    {
+        for (int col = 0; col < 8; col++)
+        {
+            // Calculate the position of the current tile
+            int x = col * 125;
+            int y = row * 125;
+
+            // Set the position of the tile sprite
+            tiles.setPosition(x, y);
+
+            // Draw the corresponding chess piece based on color and type
+            if (board.tileVector[row][col].pieceColor == Color::White)
+            {
+                if (board.tileVector[row][col].pieceType == Type::Rook)
+                {
+                    window.draw(tiles._whiteRook);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Knight)
+                {
+                    window.draw(tiles._whiteKnight);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Bishop)
+                {
+                    window.draw(tiles._whiteBishop);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Queen)
+                {
+                    window.draw(tiles._whiteQueen);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::King)
+                {
+                    window.draw(tiles._whiteKing);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Pawn)
+                {
+                    window.draw(tiles._whitePawn);
+                }
+            }
+            else if (board.tileVector[row][col].pieceColor == Color::Black)
+            {
+                if (board.tileVector[row][col].pieceType == Type::Rook)
+                {
+                    window.draw(tiles._blackRook);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Knight)
+                {
+                    window.draw(tiles._blackKnight);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Bishop)
+                {
+                    window.draw(tiles._blackBishop);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Queen)
+                {
+                    window.draw(tiles._blackQueen);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::King)
+                {
+                    window.draw(tiles._blackKing);
+                }
+                else if (board.tileVector[row][col].pieceType == Type::Pawn)
+                {
+                    window.draw(tiles._blackPawn);
+                }
+            }
+        }
+    }
+
+    window.display();
+
+    //Main game loop, while the window is open
     while (window.isOpen())
     {
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::White);
-
-        // Loop through each tile and draw the corresponding chess piece
-        for (int row = 0; row < 8; row++)
-        {
-            for (int col = 0; col < 8; col++)
             {
-                // Calculate the position of the current tile
-                int x = col * 125;
-                int y = row * 125;
-
-                // Set the position of the tile sprite
-                tiles.setPosition(x, y);
-
-                // Draw the corresponding chess piece based on color and type
-                if (board.tileVector[row][col].pieceColor == Color::White) 
+                window.close();
+            }
+            //Else if player clicks piece
+            else if (event.type == sf::Event::MouseButtonPressed)
+            {
+                //If left click piece
+                if (event.mouseButton.button == sf::Mouse::Left)
                 {
-                    if (board.tileVector[row][col].pieceType == Type::Rook) 
+                    //Get the position of the mouse click
+                    int mouseX = event.mouseButton.x;
+                    int mouseY = event.mouseButton.y;
+
+                    //Change mouse coord to board coordinates, aka tile positions
+                    int col = mouseX / 125;
+                    int row = mouseY / 125;
+
+                    //Check if the current tile has white peice, and if it is the white player's turn
+                    if (board.tileVector[row][col].pieceColor == Color::White && player == false)
                     {
-                        window.draw(tiles._whiteRook);
+
+                        // After a successful move, switch the turn to the other player
+                        player = true;
                     }
-                    else if (board.tileVector[row][col].pieceType == Type::Knight) 
+                    //Else check if the current tile has black peice, and if it is the black player's turn
+                    else if (board.tileVector[row][col].pieceColor == Color::Black && player == true)
                     {
-                        window.draw(tiles._whiteKnight);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::Bishop) 
-                    {
-                        window.draw(tiles._whiteBishop);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::Queen) 
-                    {
-                        window.draw(tiles._whiteQueen);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::King) 
-                    {
-                        window.draw(tiles._whiteKing);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::Pawn) 
-                    {
-                        window.draw(tiles._whitePawn);
-                    }
-                }
-                else if (board.tileVector[row][col].pieceColor == Color::Black) 
-                {
-                    if (board.tileVector[row][col].pieceType == Type::Rook) 
-                    {
-                        window.draw(tiles._blackRook);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::Knight) 
-                    {
-                        window.draw(tiles._blackKnight);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::Bishop) 
-                    {
-                        window.draw(tiles._blackBishop);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::Queen) 
-                    {
-                        window.draw(tiles._blackQueen);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::King) 
-                    {
-                        window.draw(tiles._blackKing);
-                    }
-                    else if (board.tileVector[row][col].pieceType == Type::Pawn) 
-                    {
-                        window.draw(tiles._blackPawn);
+
+                        // After a successful move, switch the turn to the other player
+                        player = false;
                     }
                 }
             }
+                
         }
 
-        window.display();
+        
     }
 }
 int main() {
